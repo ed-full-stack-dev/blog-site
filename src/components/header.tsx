7a19ea34d37f75e7b3541e5cb925b675f2667928
e-rojas <ed.fullstack.dev@gmail.com>
2024-11-28 19:35:08 -0400

    padding and margin fix

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,9 +4,9 @@ import React from 'react';
 function Header() {
   return (
     <header className='bg-white shadow-sm p-2'>
-      <div className=" mx-auto py-2  md:px-8 flex  justify-between items-center">
-        <div className="w-1/2 lg:w-1/3 px-2">
-          <img src="/logo.png" alt="Logo" className=" w-auto sm:px-4  sm:h-11 md:h-20" />
+      <div className=" mx-auto py-2  md:px-2 flex  justify-between items-center">
+        <div className="w-1/2 lg:w-1/3  mr-4">
+          <img src="/logo.png" alt="Logo" className=" w-auto   sm:h-11 md:h-20" />
         </div>
         <input
           type="text"
