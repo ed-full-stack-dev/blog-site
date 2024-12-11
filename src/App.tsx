41d43c79fc788ac5e498e206b5aa54b48068db1f
e-rojas <ed.fullstack.dev@gmail.com>
2024-12-11 14:26:29 -0400

    fixes for meta not rendering image

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import MainRouter from './routers/main';
-import { HelmetProvider } from 'react-helmet-async';
 
 function App() {
 
 
   return (
-    <HelmetProvider >
       <MainRouter />
-    </HelmetProvider>
   );
 }
 
