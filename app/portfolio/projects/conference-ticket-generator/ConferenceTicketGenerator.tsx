a75697971d305bc7711c4c295b094ff0905dda48
e-rojas <ed.fullstack.dev@gmail.com>
2025-02-02 00:10:03 -0400

    feature:creating basic layout for the section, content, form

--- app/portfolio/projects/conference-ticket-generator/ConferenceTicketGenerator.tsx
+++ app/portfolio/projects/conference-ticket-generator/ConferenceTicketGenerator.tsx
@@ -1,9 +1,28 @@
-import './styles.css'
+'use client';
+
+import Image from 'next/image';
+import Logo from './assets/logo-full.svg';
+import './styles.css';
 
 export default function ConferenceTicketGenerator() {
     return (
-        <section className='bg-ctgp-bg-dark text-ctgp-text-white'>
-            <h1>Portfolio: Conference Ticket Generator</h1>
+        <section className="ctgp-font _container">
+            {/* Background patterns */}
+            <div className="pattern-top"></div>
+            <div className="pattern-bottom"></div>
+
+            {/* Content */}
+            <form className='form'>
+                <div className="content">
+                    <a href="#" className="content__header-logo">
+                        <Image src={Logo} width={300} height={100} alt={'logo'} />
+                    </a>
+                    <h1 className="content__title">Your Journey to Coding Conf 2025 Starts Here!</h1>
+                    <p className="content__description">
+                        Secure your spot at next year&apos;s biggest coding conference.
+                    </p>
+                </div>
+            </form>
         </section>
-    )
+    );
 }
