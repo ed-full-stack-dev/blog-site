6318c8197d4e479144ad3f916494cd64d21abd44
e-rojas <ed.fullstack.dev@gmail.com>
2025-02-02 23:49:20 -0400

    ticketGenerator component created

--- app/portfolio/projects/conference-ticket-generator/ConferenceTicketGenerator.tsx
+++ app/portfolio/projects/conference-ticket-generator/ConferenceTicketGenerator.tsx
@@ -7,9 +7,7 @@ import { CTGformState } from '@/types/projects';
 import CtGform from './CtGform';
 import './styles.css';
 import Show from '@/components/Show';
-import { useEffect, useState } from 'react';
-import { FiGithub } from "react-icons/fi";
-import { generateTicketNumber } from '@/utils/shared';
+import TicketGenerator from './TicketGenerator';
 
 
 
@@ -38,46 +36,3 @@ export default function ConferenceTicketGenerator() {
     );
 }
 
-
-interface TicketGeneratorProps {
-    values: CTGformState;
-}
-function TicketGenerator({ values }: TicketGeneratorProps) {
-    const [avatar, setAvatar] = useState<string | null>(null);
-
-    useEffect(() => {
-        // Retrieve the uploaded image from localStorage
-        const savedImage = localStorage.getItem("uploadedImage");
-        setAvatar(savedImage || Logo); // Fallback to logo if no image is found
-    }, []);
-
-    return (
-        <div className="ticket">
-            <div className="ticket__left">
-                <div className="ticket-header">
-                    <div className="ticket-logo">
-                        <Image src={Logo} width={250} height={100} alt="logo" />
-                    </div>
-                    <span className="ticket-date">{`${new Date().toDateString()} / Austin, TX`}</span>
-                </div>
-                <div className="ticket-footer fl">
-                    {/* Display Avatar */}
-                    <Image
-                        src={avatar || Logo}
-                        width={60}
-                        height={60}
-                        alt="avatar"
-                        className="ticket-avatar"
-                    />
-                   <div className='flex flex-col text-white '>
-                   <p className="ticket-username text-2xl">{values.name}</p>
-                    <p className="ticket-github flex items-center gap-1"><FiGithub /> {values.githubUser}</p>
-                   </div>
-                </div>
-            </div>
-            <div className="ticket__right">
-               <span>{generateTicketNumber()}</span>
-            </div>
-        </div>
-    );
-}
