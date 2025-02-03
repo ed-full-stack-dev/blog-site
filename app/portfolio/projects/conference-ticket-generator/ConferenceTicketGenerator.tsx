bcc7de6772361c9262098993f08a3d25179c2138
e-rojas <ed.fullstack.dev@gmail.com>
2025-02-03 00:21:37 -0400

    refactorization into smaller components for better readability and performance

--- app/portfolio/projects/conference-ticket-generator/ConferenceTicketGenerator.tsx
+++ app/portfolio/projects/conference-ticket-generator/ConferenceTicketGenerator.tsx
@@ -1,13 +1,14 @@
 'use client';
 
-import Image from 'next/image';
-import Logo from './assets/logo-full.svg';
 import useCTGform from '@/hooks/projects/useCTGform';
 import { CTGformState } from '@/types/projects';
 import CtGform from './CtGform';
-import './styles.css';
 import Show from '@/components/Show';
 import TicketGenerator from './TicketGenerator';
+import ContentHeader from './ContentHeader';
+import ContentLogo from './ContentLogo';
+import PatternBackground from './PatternBackground';
+import './styles.css';
 
 
 
@@ -15,19 +16,24 @@ export default function ConferenceTicketGenerator() {
     const { values, errors, handleChange, handleSubmit, isSubmitSuccessful } = useCTGform<CTGformState>({ name: '', email: '', githubUser: '', file: null })
     return (
         <section className="ctgp-font _container">
-            {/* Background patterns */}
-            <div className="pattern-top"></div>
-            <div className="pattern-bottom"></div>
-
-            {/* Content */}
+            <PatternBackground />
             <div className="content">
-                <a href="#" className="content__header-logo">
-                    <Image src={Logo} width={300} height={100} alt={'logo'} />
-                </a>
-                <h1 className="content__title">Your Journey to Coding Conf 2025 Starts Here!</h1>
-                <p className="content__description">
-                    Secure your spot at next year&apos;s biggest coding conference.
-                </p>
+                <ContentLogo />
+                <Show when={isSubmitSuccessful}
+                    fallback={
+                        <ContentHeader
+                            title={<h1 className='content__title'>Your Journey to Coding Conf 2025 Starts Here!</h1>}
+                            description={<p className='content__description'>Secure your spot at next year&apos;s biggest coding conference.</p>}
+                        />
+                    }
+                >
+                    <ContentHeader
+                        title={<h1 className='content__title'>Congrats, <span className='main__title--name'>{values.name}!</span> Your ticket is ready.</h1>}
+                        description={<p className='content__description'>We have emailed your ticket to <span className='main__title--email'>{values.email}</span>  and will send updates in the run up to the event.</p>}
+                    />
+                </Show>
+
+
             </div>
             <Show when={isSubmitSuccessful} fallback={<CtGform values={values} errors={errors} handleChange={handleChange} handleSubmit={handleSubmit} />} >
                 <TicketGenerator values={values} />
@@ -35,4 +41,7 @@ export default function ConferenceTicketGenerator() {
         </section>
     );
 }
+
+
+
 
---
+++ app/portfolio/projects/conference-ticket-generator/PatternBackground.tsx
@@ -0,0 +1,9 @@
+
+export default function PatternBackground() {
+    return (
+        <>
+            <div className="pattern-top"></div>
+            <div className="pattern-bottom"></div>
+        </>
+    )
+}
