80ecc162e19a0ade72e6d64afe070e54dbd87ca1
e-rojas <ed.fullstack.dev@gmail.com>
2025-03-12 10:45:33 -0300

    refactored content section

--- components/home-page/HomeContainer.tsx
+++ components/home-page/HomeContainer.tsx
@@ -1,14 +1,21 @@
+'use client'
 // import ImageGallerySection from "./ImageGallerySection";
+import { useBlogPosts } from "@/hooks/blog";
 import ImageGallerySection from "./ImageGallerySection";
 import IntroSection from "./IntroSection";
+import ContentSection from "./ContentSection";
 
 
 export default function HomeContainer() {
-
+    const { data, loading, error } = useBlogPosts();
+    if(loading) return <p>Loading...</p>
+    if(error) return <p>Error Loading Content</p>
+    console.log('data', data)
     return (
         <div className="home-page">
             <IntroSection />
             <ImageGallerySection />
+            <ContentSection data={data} />
         </div>
 
     )
