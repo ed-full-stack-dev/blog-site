1671f553fa1e527162073039602424f958c7b91c
e-rojas <ed.fullstack.dev@gmail.com>
2025-03-12 09:45:56 -0300

    section gallery added

---
+++ components/article-card/index.tsx
@@ -0,0 +1,24 @@
+import { FaChevronRight } from "react-icons/fa";
+import DateDisplay from '../DateDisplay'
+import './styles.css'
+interface ArticleCardProps {
+    date: string;
+    formattedDate: string;
+    title: string;
+    description:string;
+    link?:string
+}
+export default function ArticleCard({date, formattedDate, title, description}:ArticleCardProps) {
+    return (
+        <article className="article-card">
+            <DateDisplay date={date} formattedDate={formattedDate} />
+            <h2 className="article-card__title">{title}</h2>
+            <p className="article-card__description">{description}</p>
+            <div className='article-card__footer'>
+                <span className="article-card__read-more">Read article
+                    <FaChevronRight className="article-card__icon" />
+                </span>
+            </div>
+        </article>
+    )
+}
--- components/home-page/HomeContainer.tsx
+++ components/home-page/HomeContainer.tsx
@@ -1,9 +1,14 @@
+// import ImageGallerySection from "./ImageGallerySection";
+import ImageGallerySection from "./ImageGallerySection";
 import IntroSection from "./IntroSection";
 
+
 export default function HomeContainer() {
+
     return (
         <div className="home-page">
             <IntroSection />
+            <ImageGallerySection />
         </div>
 
     )
